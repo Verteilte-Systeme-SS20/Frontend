ff2ee722e8dfb4a9320b4d8c07252a05a87801e7
Jonas Müller <jonas.mueller.contact@gmail.com>
2020-05-04 11:19:38 +0200

    Spring initializr + IDEA projekt.

---
+++ src/main/java/de/reutlingenuniversity/vs_frontend/VsFrontendApplication.java
@@ -0,0 +1,13 @@
+package de.reutlingenuniversity.vs_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/reutlingenuniversity/vs_frontend/VsFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.reutlingenuniversity.vs_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
